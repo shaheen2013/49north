ea1c9abacdaae5142482a7ecbaf6bbaff5809460
dsvinfosolutions <vinay@dsvinfosolutions.com>
2020-01-07 17:55:42 +0530

    made changes agreement migration

--- app/Agreement.php
+++ app/Agreement.php
@@ -3,7 +3,13 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\SoftDeletes;
 class Agreement extends Model {
+	use  SoftDeletes;
+
+	public $table = "agreements";
+
     protected $guarded = [];
+
+    
 }
