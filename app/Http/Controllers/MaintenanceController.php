160bdaa61d1e70d1be9f892f4140752076be0178
dsvinfosolutions <vinay@dsvinfosolutions.com>
2020-01-09 20:10:19 +0530

    Made new controller for maintenance

---
+++ app/Http/Controllers/MaintenanceController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MaintenanceController extends Controller
+{
+    function Maintenance_list()
+    {
+    	echo 'Maintenance page';
+    }
+}
