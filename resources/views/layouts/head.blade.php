2b11646d0e1f4a35688890c7251b018808ca3908
unknown <vinay@dsvinfosolutions.com>
2020-01-10 20:07:05 +0530

    removed cdn link of fontawesome

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -11,7 +11,7 @@
   <link href="{{asset('css/bootstrap.min.css')}}" rel="stylesheet">
   <!-- Custom styles-->
   <link href="{{asset('css/style.css')}}" rel="stylesheet">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link href="{{asset('css/fontawesome.min.css')}}" rel="stylesheet">
 
     <!-- Bootstrap core JavaScript -->
   <script src="{{asset('js/jquery.min.js')}}"></script>
