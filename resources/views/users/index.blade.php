7dd2f0d1ff6dc3e9d8689406344599e1bd93b42f
Sean Glendinning <sean@fitnessmanagement.ca>
2020-01-18 20:53:14 -0800

    - update permissions to function the way it does on our other sites and remove old permissions

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,14 +1,11 @@
-{{-- \resources\views\users\index.blade.php --}}
 @extends('layouts.main')
 
 @section('title', 'Users')
 
 @section('content1')
 
-
-    <h1><i class="fa fa-users"></i> User Administration <a href="{{ route('roles.index') }}"
-                                                           class="btn btn-default pull-right">Roles</a>
-        <a href="{{ route('permissions.index') }}" class="btn btn-default pull-right">Permissions</a></h1>
+    <h1>
+        <a href="{{ route('admin.permissions.index') }}" class="btn btn-default pull-right">Permissions</a></h1>
     <hr>
 
     <a href="{{ route('users.create') }}" class="btn btn-success">Add User</a>
