43ca079dbb6b286f7278b1e1d3fa1d0f76dcd48b
Sean Glendinning <sean@fitnessmanagement.ca>
2020-01-18 20:13:49 -0800

    - add "admin" route file

---
+++ routes/admin.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/**
+ * @todo Update these controllers to match the rest (location and naming convention)
+ */
+Route::group(['middleware' => ['auth', 'isAdmin']], function () {
+    Route::resource('users', 'UserController');
+    Route::get('force-login/{user}', 'UserController@forceLogin')->name('force-login');
+});
+
+
+Route::group(['namespace' => 'Admin', 'prefix' => 'admin', 'as' => 'admin.'], function () {
+    Route::get('pay-statements', 'AdminPaystatementController@paystatement')->name('pay-statements');
+
+    Route::get('agreement', 'AdminAgreementController@agreementlist')->name('agreement-list');
+
+    Route::get('registration', 'AdminController@index')->name('registration');
+    Route::get('expences_report', 'AdminController@expences_report')->name('expense-report');
+    Route::get('milegebook', 'AdminController@milegebook')->name('mileage-book');
+    Route::get('tech_maintanance', 'AdminController@tech_maintanance')->name('tech-maintenance');
+    Route::get('timeoff', 'AdminController@timeoff')->name('timeoff');
+    Route::get('reportconcern', 'AdminController@reportconcern')->name('report-concern');
+    Route::post('add_registration', 'AdminController@add_registration')->name('add-registration');
+
+    // permissions
+    Route::resource('permissions', 'AdminPermissionsController');
+    Route::resource('roles', 'AdminRolesController');
+});
