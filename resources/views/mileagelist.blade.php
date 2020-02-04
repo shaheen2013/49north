fb45aa61b25005a7c9d1a21afda22b35bc146989
Sean Glendinning <sean@fitnessmanagement.ca>
2020-02-04 10:02:33 -0800

    - JS function formatDate does not need to be redeclared

--- resources/views/mileagelist.blade.php
+++ resources/views/mileagelist.blade.php
@@ -574,19 +574,5 @@
             });
         }
 
-        // Format date
-        function formatDate(date) {
-            const d = new Date(date), year = d.getFullYear();
-            let month = '' + (d.getMonth() + 1),
-                day = '' + d.getDate();
-
-            if (month.length < 2)
-                month = '0' + month;
-            if (day.length < 2)
-                day = '0' + day;
-
-            return [year, month, day].join('-');
-        }
-
     </script>
 @endpush
